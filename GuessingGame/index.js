58ba912ffccdc87177a40a800fbb90882519de3b
PetroMatkovskyi <75044180+PetroMatkovskyi@users.noreply.github.com>
2021-03-28 02:15:50 +0200

    rename the function

--- GuessingGame/index.js
+++ GuessingGame/index.js
@@ -11,25 +11,25 @@ let randomValue;
 if (!questions) {
     console.log(`You did not become a millionaire`)
 } else {
-    random()
-    enter()
+    randomNumber()
+    enterUserNumber()
 }
 
-function random() {
+function randomNumber() {
     randomValue = Math.ceil(Math.random() * 5 * stepForRange);
     console.log(randomValue)
 }
 
-function enter() {
+function enterUserNumber() {
     possiblePrize = prize[attemptsLeft - 1] * stepForPrize;
     userEnter = prompt(`Enter a number form 0 to ${5 * stepForRange}
   Attempts left: ${attemptsLeft}
   Total prize: ${totalPrize}
   Possibel prize on curent attempt: ${possiblePrize}`, '')
-    game()
+    gameBody()
 };
 
-function game() {
+function gameBody() {
     attemptsLeft--
     if (userEnter == randomValue) {
         totalPrize += possiblePrize;
@@ -39,29 +39,29 @@ function game() {
         prize.forEach(item => item * 3)
         let nextLevel = confirm('Do you want to continue the game?')
         if (nextLevel) {
-            random()
-            enter()
+            randomNumber()
+            enterUserNumber()
         } else {
             console.log(`Your prize is: ${totalPrize}`)
-            newGame()
+            newgameBody()
         }
     } else if (attemptsLeft > 0) {
-        enter()
+        enterUserNumber()
     } else {
         console.log(`Your prize is: ${totalPrize}`)
-        newGame()
+        newgameBody()
     }
 }
 
-function newGame() {
+function newgameBody() {
     let newGame = confirm('Do you want to play again?')
     if (newGame) {
         attemptsLeft = 3;
         stepForPrize = 1;
         stepForRange = 1;
         totalPrize = 0;
-        random()
-        enter()
+        randomNumber()
+        enterUserNumber()
     } else {
         document.write(`<h1>THE END</h1>`)
     }
